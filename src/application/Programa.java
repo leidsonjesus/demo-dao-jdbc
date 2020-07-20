4e20a5d124b3fbefc7de5b56f166c1dd8f97fe8d
Leidson Jesus <leidson18e@gmail.com>
2020-07-20 19:30:43 -0300

    Saller class

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -1,14 +1,20 @@
 package application;
 
+import java.util.Date;
+
 import model.entidade.Department;
+import model.entidade.Saller;
 
 public class Programa {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+ 
 		Department dp = new Department(1, " Books");
 		System.out.println(dp);
 
+		Saller saller = new Saller(12, "Leidson", "leidson333@gmail.com", new Date(), 15000.00, dp);
+		System.out.println(saller);
 	}
+	
 
 }
