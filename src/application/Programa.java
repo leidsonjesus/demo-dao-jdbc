fbf9c876edba7b34e9b49ed78f12df9395010788
Leidson Jesus <leidson18e@gmail.com>
2020-07-20 19:12:41 -0300

    departmento class

---
+++ src/application/Programa.java
@@ -0,0 +1,14 @@
+package application;
+
+import model.entidade.Department;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Department dp = new Department(1, " Books");
+		System.out.println(dp);
+
+	}
+
+}
